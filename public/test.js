f930656451af770df6e7d34e2e5af1885e5f20a6
Hiroya-Kato <a12.anr7@g.chuo-u.ac.jp>
2016-09-13 19:27:35 +0900

    2st commit

---
+++ public/test.js
@@ -0,0 +1,17 @@
+$(function(){
+    $('#button').bind("click",function(){
+        var re = new RegExp($('#search').val());
+        $('#result tbody tr').each(function(){
+            var txt = $(this).find("td:eq(0)").html();
+            if(txt.match(re) != null){
+                $(this).show();
+            }else{
+                $(this).hide();
+            }
+        });
+    });
+
+    $('#button2').bind("click",function(){
+        $('#result tr').show();
+    });
+});
